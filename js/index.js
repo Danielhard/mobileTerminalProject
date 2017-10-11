cc4374210cb2466cdc4fb34c8f927e750963bdeb
YJH <m17854239497@outlook.com>
2017-10-11 21:16:28 +0800

    首页

--- js/index.js
+++ js/index.js
@@ -7,4 +7,11 @@
     // 如果需要分页器
     pagination: '.swiper-pagination'
   })
+
+  // 底部导航的js代码,点击那个跳转到哪一页并改变相应的li的样式
+  $('.index-nav-list').each(function(i,item){
+    $(item).on('click',function(){
+      $(this).addClass('selected').siblings().removeClass('selected');
+    })
+  });
 })();
