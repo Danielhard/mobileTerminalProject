46a0425bbe444e64dde4111472d15945b961141b
YJH <m17854239497@outlook.com>
2017-10-11 16:36:48 +0800

    add frame file

---
+++ js/index.js
@@ -0,0 +1,10 @@
+(function(){
+  // 利用swiper制作首页banner
+  var mySwiper = new Swiper('.swiper-container', {
+    // 自动播放
+    autoplay: 6000,
+    loop: true,
+    // 如果需要分页器
+    pagination: '.swiper-pagination'
+  })
+})();
